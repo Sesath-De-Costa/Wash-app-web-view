1ef8d57eb6a9351f5819d24c6d3eda7f85dd8f02
sesath <sesathdecosta@gmail.com>
2021-02-23 16:42:26 +0530

    Add deliver-agents component

---
+++ src/app/view/deliver-agents/deliver-agents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deliver-agents',
+  templateUrl: './deliver-agents.component.html',
+  styleUrls: ['./deliver-agents.component.scss']
+})
+export class DeliverAgentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
