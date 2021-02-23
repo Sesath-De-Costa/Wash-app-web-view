c803e2f0cec98116205a3f9c29907b3b02eb33f5
sesath <sesathdecosta@gmail.com>
2021-02-23 16:37:11 +0530

    Add dashboard component

---
+++ src/app/view/dashboard/dashboard.component.ts
@@ -0,0 +1,35 @@
+import {Component} from '@angular/core';
+import {map} from 'rxjs/operators';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({matches}) => {
+      if (matches) {
+        return [
+          {title: 'Reports', cols: 1, rows: 1},
+          {title: 'Card 2', cols: 1, rows: 1},
+          {title: 'Card 3', cols: 1, rows: 1},
+          {title: 'Card 4', cols: 1, rows: 1}
+        ];
+      }
+
+      return [
+        {title: 'Reports', cols: 2, rows: 1},
+        {title: 'Card 2', cols: 1, rows: 1},
+        {title: 'Card 3', cols: 1, rows: 2},
+        {title: 'Card 4', cols: 1, rows: 1}
+      ];
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver, private router: Router) {
+  }
+}
