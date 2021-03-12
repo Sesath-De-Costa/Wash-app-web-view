d98e8a2c4f9fbf46c73779fde280a01e4196cb07
sesath <sesathdecosta@gmail.com>
2021-03-12 16:05:08 +0530

    Add request.service

---
+++ src/app/service/request.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {Request} from '../model/Request';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getUser(query: string): Observable<Request> {
+    const token = sessionStorage.getItem('token');
+    if (token === null) {
+      return throwError('Invalid token');
+    } else {
+      const httpParams = new HttpParams().append('q', query);
+      return this.http.get<Request>(`http://localhost:8080/washapp/api/v1/requsets?q=${query}`, {
+        params: httpParams,
+        headers: new HttpHeaders()
+          .append('Authorization', `Bearer ${token}`)
+      });
+    }
+  }
+
+  getAllRequests(): Observable<Array<Request>> {
+    const token = sessionStorage.getItem('token');
+    if (token === null) {
+      return throwError('Invalid token');
+    } else {
+      return this.http.get<Array<Request>>(`http://localhost:8080/api/v1/requsets/all`, {
+        headers: new HttpHeaders()
+          .append('Authorization', `Bearer ${token}`)
+      });
+    }
+  }
+}
